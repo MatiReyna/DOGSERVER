7056303706bcf7cd15f260a469ef24093d45cabb
Matias Reyna <matiireyna@hotmail.com>
2024-06-24 10:07:09 -0300

    Conexion a la DB: exitoso

---
+++ api/src/DB_connection.js
@@ -0,0 +1,20 @@
+require('dotenv').config();  // Carga las variables de entorno.
+const { Sequelize } = require('sequelize');
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env  // Variables de entorno del archivo .env.
+
+
+//* A continuación se deberian importar los modelos.
+
+// Configuración de sequelize.
+const sequelize = new Sequelize(
+    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
+    { logging: false, native: false }
+);
+
+//* A continuación se deben ejecutar los modelos, pasandole por parametro a sequelize.
+
+//* A continuación se debe relacionar los modelos.
+
+module.exports = {
+    conn: sequelize
+}
--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,12 @@
 const server  = require('./app');  // Importamos el servidor.
+const { conn } = require('./DB_connection');  // Importamos la conexión a la base de datos.
 
-server.listen(3001, () => {  // Lo ponemos a escuchar.
-    console.log('Server listening on PORT 3001')  // Se imprime en la consola.
-});
+conn.sync({ force: true }).then(() => {
+    console.log('Connection established')  // Si todo sale bien, la conexión a la DB se hizo exitosa.
+    server.listen(3001, () => {  // Lo ponemos a escuchar.
+        console.log('Server listening on PORT 3001')  // Se imprime en la consola.
+    })
+})
+    .catch((error) => { console.log('Error al conectar', error) })  // Maneja error de sincronización o inicialización del server.
+
+//TODO: SINCRONIZAMOS LA DB conn.sync({ force: true }) LO QUE INDICA QUE SE RECREA LA DB EN CADA REINICIO DEL SERVIDOR.
