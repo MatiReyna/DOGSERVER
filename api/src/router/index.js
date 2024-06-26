ef0ecc6d58bd51b49e4cd2b321182c5dff308d64
Matias Reyna <matiireyna@hotmail.com>
2024-06-26 13:35:23 -0300

    Configuracion de las rutas de temperamentos

--- api/src/router/index.js
+++ api/src/router/index.js
@@ -1,5 +1,8 @@
 const { Router } = require('express');
+const temperamentRouter = require('./temperamentRouter');
 
 const router = Router();
+
+router.use('/temperament', temperamentRouter);
 
 module.exports = router;
