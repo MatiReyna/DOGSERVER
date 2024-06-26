a39d91664f3e5fef479ebc593171159118535a74
Matias Reyna <matiireyna@hotmail.com>
2024-06-26 13:48:00 -0300

    Ruta que obtiene los temperamentos: done

--- api/src/router/temperamentRouter.js
+++ api/src/router/temperamentRouter.js
@@ -1,5 +1,20 @@
 const { Router } = require('express');
+const { getTemperaments } = require('../controller/getTemperament');
 
 const temperamentRouter = Router();
+
+temperamentRouter.get('/', async (request, response) => {
+    try {
+        const allTemperaments = await getTemperaments()  // Ejecutamos el controlador de temperamentos.
+
+        if (allTemperaments) {
+            return response.status(200).json(allTemperaments);
+        } else {
+            return res.status(404).json('No Temperaments found');
+        }
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
 
 module.exports = temperamentRouter;
