d8fd75ece580e2cac110c4866ea9e1ae35185bd5
Matias Reyna <matiireyna@hotmail.com>
2024-06-26 13:24:56 -0300

    Creacion del controller sobre temperamentos

---
+++ api/src/controller/getTemperament.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const { Temperament } = require('../DB_connection');
+require('dotenv').config();
+const { API_KEY } = process.env;
+
+const getTemperaments = async () => {  // Función que obtiene los temperamentos de la API y los guarda en la DB.
+
+    const temperamentDataBase = await Temperament.findAll();  // Busca todos los temperamentos de la DB.
+
+    if (temperamentDataBase.length) {  // Si, cargo todos los temperamentos, que lo retorne.
+        return temperamentDataBase
+    } else {
+        // Extraemos toda la información de la API.
+        const temperamentApi = (await axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`)).data;
+
+        let auxiliar = temperamentApi.flatMap((dog) => (dog.temperament || '').split(', ').map((temp) => temp.trim())).filter(Boolean);
+
+        const temperamentOrder = [...new Set(auxiliar)].sort();
+
+        // Creamos los temperamentos en la DB.
+        const createdTemperament = await Temperament.bulkCreate(
+            temperamentOrder.map(temp => ({ name: temp }))
+        );
+        return createdTemperament.map((temp) => temp.name);
+    }
+};
+
+module.exports = {
+    getTemperaments
+}
