a52852af8070cde17da74803d440ba836bf92afe
Matias Reyna <matiireyna@hotmail.com>
2024-06-28 10:45:15 -0300

    Ruta que obtiene los perros: done

--- api/src/router/dogRouter.js
+++ api/src/router/dogRouter.js
@@ -1,5 +1,18 @@
 const { Router } = require('express');
+const { getAllDogs } = require('../controller/getAllDog');
+const { getTemperaments } = require('../controller/getTemperament');
 
 const dogRouter = Router();
+
+dogRouter.get('/', async (request, response) => {
+    try {
+        await getTemperaments();
+        
+        const allDogs = await getAllDogs();
+        return response.status(200).json(allDogs);
+    } catch (error) {
+        return res.status(500).json({ error: 'Internal Server Error', details: error.message });
+    }
+});
 
 module.exports = dogRouter;
