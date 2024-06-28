c0ed25a49a4e18fb8c4702fca02d849a30b31a16
Matias Reyna <matiireyna@hotmail.com>
2024-06-28 10:37:44 -0300

    Configuracion de las rutas de perros

---
+++ api/src/router/dogRouter.js
@@ -0,0 +1,5 @@
+const { Router } = require('express');
+
+const dogRouter = Router();
+
+module.exports = dogRouter;
--- api/src/router/index.js
+++ api/src/router/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const temperamentRouter = require('./temperamentRouter');
+const dogRouter = require('./dogRouter');
 
 const router = Router();
 
 router.use('/temperament', temperamentRouter);
+router.use('/dog', dogRouter);
 
 module.exports = router;
