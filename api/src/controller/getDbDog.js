a93eafa4ce4aca960c5cb58a34a73427f5ac020e
Matias Reyna <matiireyna@hotmail.com>
2024-06-28 10:25:45 -0300

    Controlador que trae los perros de la DB

---
+++ api/src/controller/getDbDog.js
@@ -0,0 +1,31 @@
+const { Dog, Temperament } = require('../DB_connection');
+
+const getDbDogs = async () => {  // Función que devuelve una lista de los perros de la DB.
+
+    const dataBaseDog = await Dog.findAll({
+        include: {  // Incluye información adicional del modelo de temperamentos.
+            model: Temperament,
+            attributes: ['name'],  // Solo me quedo con los nombre de los temperamentos.
+            through: {  // Se especifica que es la tabla intermedia.
+                attributes: []  // Para que no incluya ninguna propiedad de esa tabla.
+            }
+        }
+    });
+
+    return dataBaseDog.map((dog) => {  // Mapea los perros encontrados y crea un objeto por cada uno.
+        return {
+            id: dog.id,
+            name: dog.name,
+            height: dog.height,
+            weight: dog.weight,
+            life_span: dog.life_span,
+            image: dog.image,
+            temperaments: dog.Temperament.map((t) => t.name),
+            from: 'DATA BASE'  // Le creo un campo para saber de donde proviene.
+        }
+    });
+};
+
+module.exports = {
+    getDbDogs
+}
