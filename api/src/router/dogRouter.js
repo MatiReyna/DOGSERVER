997134129c5f176b083ae1f6424f0c98b1a241a7
Matias Reyna <matiireyna@hotmail.com>
2024-07-01 17:20:12 -0300

    Ruta que obtiene los perros por ID: done

--- api/src/router/dogRouter.js
+++ api/src/router/dogRouter.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const { getAllDogs } = require('../controller/getAllDog');
 const { getTemperaments } = require('../controller/getTemperament');
+const { getById } = require('../controller/getById');
 
 const dogRouter = Router();
 
@@ -12,6 +13,17 @@ dogRouter.get('/', async (request, response) => {
         return response.status(200).json(allDogs);
     } catch (error) {
         return res.status(500).json({ error: 'Internal Server Error', details: error.message });
+    }
+});
+
+dogRouter.get('/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+
+        const dogFind = await getById(id);
+        return response.status(200).json(dogFind);
+    } catch (error) {
+        return res.status(500).send({ error: error.message })
     }
 });
 
