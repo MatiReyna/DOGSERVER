65aed9d242c32b0e327d6b688476970fe307a000
Matias Reyna <matiireyna@hotmail.com>
2024-07-16 10:37:36 -0300

    Solucion para incluir los temperamentos

--- api/src/controller/getAllDog.js
+++ api/src/controller/getAllDog.js
@@ -1,3 +1,4 @@
+const { Dog, Temperament } = require('../DB_connection');
 const { getApiDogs } = require('./getApiDog');
 const { getDbDogs } = require('./getDbDog');
 
@@ -7,10 +8,23 @@ const getAllDogs = async () => {
     const [ apiDogs, dbDogs ] = await Promise.all([ getApiDogs(), getDbDogs() ]);
 
     // Unimos las dos fuentes de datos para que sea una sola lista.
-    // const allDogs = dbDogs.concat(apiDogs);
-    const allDogs = [...dbDogs, ...apiDogs];
+    const allDogs = dbDogs.concat(apiDogs);
 
-    return allDogs;  // Se retornan todos los perros.
+    const dogWithTemperaments = await Promise.all(
+        allDogs.map(async (dog) => {
+            if (dog.from === 'DB') {
+                const dbDog = await Dog.findByPk(dog.id, { include: Temperament })  // Si el perro es de la base de datos, obtenemos los temperamentos.
+                return {
+                    ...dog,
+                    temperaments: dbDog.Temperaments.map((t) => t.name)
+                }
+            } else {
+                return dog;
+            }
+        })
+    );
+
+    return dogWithTemperaments;  // Se retornan todos los perros.
 };
 
 module.exports = {
