49dbb156eaae248bb49fa7cd62db382c40b8e48e
Matias Reyna <matiireyna@hotmail.com>
2024-07-19 19:33:25 -0300

    Comentarios en el codigo

--- api/src/app.js
+++ api/src/app.js
@@ -18,7 +18,7 @@ server.use((request, response, next) => {
         'GET, POST, OPTIONS, PUT, DELETE'
     );
     next();
-});  // Comentario para mantener los commit.
+});  // Comentario para mantener los commit. Otro comentario.
 
 server.use('/', router);  // Configuracion de rutas.
 
