eda563089a7ba90a22df29253db203df289acd7f
Matias Reyna <matiireyna@hotmail.com>
2024-08-22 19:34:32 -0300

    Correccion en la conexion con la base de datos

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,12 @@
 const server  = require('./app');  // Importamos el servidor.
 const { conn } = require('./DB_connection');  // Importamos la conexión a la base de datos.
 
+const PORT = process.env.PORT || 3001;
+
 conn.sync({ force: true }).then(() => {
     console.log('Connection established')  // Si todo sale bien, la conexión a la DB se hizo exitosa.
-    server.listen(3001, () => {  // Lo ponemos a escuchar.
-        console.log('Server listening on PORT 3001')  // Se imprime en la consola.
+    server.listen(PORT, () => {  // Lo ponemos a escuchar.
+        console.log(`Server listening on PORT ${PORT}`)  // Se imprime en la consola.
     })
 }).catch((error) => { console.log('Error al conectar', error) })  // Maneja error de sincronización o inicialización del server.
 
