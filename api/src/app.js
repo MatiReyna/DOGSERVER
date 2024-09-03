13225f7e4396baebaba8f4910888fb31f77f5dc7
Matias Reyna <matiireyna@hotmail.com>
2024-09-02 22:07:43 -0300

    Correccion en la dependencia CORS

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const cros = require('cros');
+const cors = require('cors');  // Middleware para manejo de CORS.
 const morgan = require('morgan');  // Middleware de registro de solicitud HTTP.
 const router = require('./router/index');  // Enrutador.
 
@@ -7,20 +7,7 @@ const server = express();  // Instancia del servidor.
 
 server.use(morgan('dev'));
 server.use(express.json());  // Solicitud en formato JSON.
-server.use((request, response, next) => {
-    response.header('Access-Control-Allow-Origin', '*');
-    response.header('Access-Control-Allow-Credentials', 'true');
-    response.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    response.header(
-        'Access-Control-Allow-Methods',
-        'GET, POST, OPTIONS, PUT, DELETE'
-    );
-    next();
-}); 
-// server.use(cros({ credentials: true, origin: '*' }));  // Usando el middleware cros.
+server.use(cors({ credentials: true, origin: '*' }));  // Usando el middleware cros.
 
 server.use('/', router);  // Configuracion de rutas.
 
